fe84d80da87b3a6fb8196c04651a950fd0c6f668
lailahgrant <lailahgrant19@gmail.com>
2021-08-22 19:39:37 +0300

    moment.js functionality

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+import moment from 'https://cdn.jsdelivr.net/npm/vue-moment@4.0.0-0/vue-moment.min.js'
 
 
 
@@ -7,6 +7,14 @@ var app = new Vue({
     
     data: {
         heading: "Calendar",
-        
-    }
+        today: moment(),
+        dateContext: moment(),
+        days: ['S', 'M', 'T', 'W', 'T', 'F','S']
+    },
+    
+    methods: {
+        nextMonth() {
+            this.dateContext = moment(this.dateContext).add(1, 'month');
+        }
+    },
 })
